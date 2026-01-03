021f43c64bdf6fc34d4d96c2cfdd05c71f62cfd3
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-01-03 22:13:27 +0330

    add fragment model files

---
+++ app/Http/Controllers/FragmentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fragment;
+use App\Http\Requests\StoreFragmentRequest;
+use App\Http\Requests\UpdateFragmentRequest;
+
+class FragmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFragmentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Fragment $fragment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Fragment $fragment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFragmentRequest $request, Fragment $fragment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Fragment $fragment)
+    {
+        //
+    }
+}
